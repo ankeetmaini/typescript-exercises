e36236cd1190a026537eafeb12d73aa8e53cf918
Ankeet Maini <ankeet.maini@gmail.com>
2020-05-21 23:34:13 +0530

    solution 4

--- exercises/exercise-04/index.ts
+++ exercises/exercise-04/index.ts
@@ -95,9 +95,10 @@ function logPerson(person: Person) {
     console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
 }
 
-function filterUsers(persons: Person[], criteria: User): User[] {
+type Filter = Partial<Omit<User, 'type'>>;
+function filterUsers(persons: Person[], criteria: Filter): User[] {
     return persons.filter(isUser).filter((user) => {
-        let criteriaKeys = Object.keys(criteria) as (keyof User)[];
+        let criteriaKeys = Object.keys(criteria) as (keyof Filter)[];
         return criteriaKeys.every((fieldName) => {
             return user[fieldName] === criteria[fieldName];
         });
